49f719b3ce87c90833ed2a83d02cecf8a38dddb0
betolix <betolix@gmail.com>
2024-02-13 17:21:40 -0500

    Creation of components.MyComponents.kt / Composables SpacerComponent and TextComponent

--- app/src/main/java/io/h3llo/eco_eats/presentation/components/MyComponents.kt
+++ app/src/main/java/io/h3llo/eco_eats/presentation/components/MyComponents.kt
@@ -2,13 +2,17 @@ package io.h3llo.eco_eats.presentation.components
 
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import io.h3llo.eco_eats.R
+
 
 
 @Composable
@@ -22,4 +26,25 @@ fun ImageComponent(
         contentDescription = description,
         modifier = modifier
     )
+}
+
+@Composable
+fun SpacerComponent(
+    modifier: Modifier = Modifier
+){
+    Spacer(modifier = modifier)
+}
+
+@Composable
+fun TextComponent(
+    modifier: Modifier = Modifier,
+    text:String,
+    style: TextStyle
+){
+    Text(
+        modifier = modifier,
+        text = text,
+        style = style
+    )
+
 }
--- app/src/main/java/io/h3llo/eco_eats/presentation/welcome/WelcomeScreen.kt
+++ app/src/main/java/io/h3llo/eco_eats/presentation/welcome/WelcomeScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -14,12 +15,18 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import io.h3llo.eco_eats.R
 import io.h3llo.eco_eats.presentation.components.ImageComponent
+import io.h3llo.eco_eats.presentation.components.SpacerComponent
+import io.h3llo.eco_eats.presentation.components.TextComponent
 
 @Composable
 fun WelcomeScreen(
@@ -65,6 +72,18 @@ fun WelcomeHeader() {
             modifier = Modifier
                 .fillMaxWidth()
                 .size(25.dp))
+        SpacerComponent(modifier = Modifier.height(8.dp))
+
+        TextComponent(
+            modifier = Modifier.fillMaxWidth(),
+            text = "Una vida saludable",
+            style = TextStyle(
+                textAlign = TextAlign.Center,
+                color = Color.Black,
+                fontSize = 15.sp,
+                fontWeight = FontWeight.Normal
+            )
+        )
     }
 }
 
