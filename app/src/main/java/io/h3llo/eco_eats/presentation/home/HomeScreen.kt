defa374e2d4806d47d83f3d703632608d49fcf26
betolix <betolix@gmail.com>
2024-03-04 18:25:43 -0500

    Logic for the bottomBarItems to highlight when pressed. Badge and News for the bottomBarItems

--- app/src/main/java/io/h3llo/eco_eats/presentation/home/HomeScreen.kt
+++ app/src/main/java/io/h3llo/eco_eats/presentation/home/HomeScreen.kt
@@ -13,6 +13,8 @@ import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material.icons.outlined.Home
 import androidx.compose.material.icons.outlined.Search
 import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.material3.Badge
+import androidx.compose.material3.BadgedBox
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
@@ -21,6 +23,10 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -49,7 +55,7 @@ fun HomeScreen() {
             route = ""
         )
         ,BottomNavigationItem(
-            title = "Dishes",
+            title = "Settings",
             selectedIcon = Icons.Filled.Settings,
             unselectedIcon = Icons.Outlined.Settings,
             hasNews = true,
@@ -57,19 +63,43 @@ fun HomeScreen() {
         )
     )
 
+    var selectedItemIndex by remember {
+        mutableStateOf(0)
+    }
+
     Scaffold (
         modifier = Modifier.fillMaxSize(),
         bottomBar = {
             NavigationBar {
                 items.forEachIndexed { index, item ->
                     NavigationBarItem(
-                        selected = false,
-                        onClick = { /*TODO*/ },
+                        selected = selectedItemIndex == index,
+                        onClick = {
+                            selectedItemIndex = index
+                        },
                         icon = {
-                               Icon(
-                                   imageVector = item.unselectedIcon,
-                                   contentDescription = item.title
-                                   )
+
+                            BadgedBox(
+                                badge = {
+                                    if(item.badgeCount != null){
+                                        Badge {
+                                            Text(text = item.badgeCount.toString())
+                                        }
+                                    } else if (item.hasNews){
+                                        Badge()
+                                    }
+                                }
+                            ) {
+                                Icon(
+                                    imageVector =if (index == selectedItemIndex)
+                                        item.selectedIcon
+                                    else
+                                        item.unselectedIcon,
+                                    contentDescription = item.title
+                                )
+                            }
+
+
                         },
                         label = {
                             Text(text = item.title)
